b032619967e9e80aab7debf33adddfe77be2ff26
VictorDevvs <victorsantosdevvs@gmail.com>
2025-11-17 09:14:08 -0300

    criando o repositório para a entidade

---
+++ src/main/java/security/jwt/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package security.jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import security.jwt.domain.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
