02fa2dc7fdf15fd5dad5976427efe1ad099123a7
VictorDevvs <victorsantosdevvs@gmail.com>
2026-02-13 22:40:11 -0300

    configurando o CORS com o link do frontend em produção

--- src/main/java/security/jwt/config/CorsConfig.java
+++ src/main/java/security/jwt/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000")
+                        .allowedOrigins("https://seguranca-jwt-frontend.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
